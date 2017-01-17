f156028db82ecbf86b1ab570145b3b3f2518c79b
Svegin <svegin@Svegins-Mini.attlocal.net>
2017-01-17 14:00:34 -0600

    add check secret word in speach

--- master.js
+++ master.js
@@ -7,8 +7,25 @@ let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
 
-recognition.addEventListener('result', e =>{
-console.log(e);
+recognition.addEventListener('result', e => {
+
+const transcript = Array.from(e.results)
+  .map(result => result[0])
+  .map(result => result.transcript)
+  .join('')
+
+  p.textContent = transcript;
+  if(e.results[0].isFinal){
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
+  // check secret word in speach
+  if(transcript.includes('Давай выпьем чаю')){
+    console.log('давай!!!');
+  }
+  console.log(transcript);
 });
+
+recognition.addEventListener('end', recognition.start);
 
 recognition.start();
